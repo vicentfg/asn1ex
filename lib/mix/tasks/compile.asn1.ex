d7a754a06dc645679b8280969938d2ddbe6de0b4
Calancea Daniel <d4no0007@gmail.com>
2019-12-20 14:33:24 +0200

    Changed from Mix.Project.config to Application.get_env

--- lib/mix/tasks/compile.asn1.ex
+++ lib/mix/tasks/compile.asn1.ex
@@ -37,11 +37,10 @@ defmodule Mix.Tasks.Compile.Asn1 do
   def run(args) do
     {opts, _, _} = OptionParser.parse(args, switches: [force: :boolean])
 
-    project      = Mix.Project.config
-    source_paths = project[:asn1_paths] || ["asn1"]
-    dest_paths    = project[:erlc_paths]
+    source_paths = Application.get_env(:asn1ex, :asn1_paths) || ["asn1"]
+    dest_paths    = Application.get_env(:asn1ex, :erlc_paths) || ["src"]
 #    mappings     = Enum.zip(source_paths, dest_paths)
-    options      = project[:asn1_options] || []
+    options      = Application.get_env(:asn1ex, :asn1_options) || []
 
     build_dest()
 
